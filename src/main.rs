689543c99ba2d1c394e083bdddef40c017e7f3a8
wyoheiii <seikimatu4649@gmail.com>
2024-11-18 19:52:41 +0900

    add main

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+extern crate alloc;
+
+use crate::alloc::string::ToString;
+use net_wasabi::http::HttpClient;
+use noli::prelude::*;
+
+fn main() -> u64 {
+  let client = HttpClient::new();
+  match client.get("example.com".to_string(), 80, "/".to_string()) {
+    Ok(response) => {
+      println!("Response: {:?}", response);
+    },
+    Err(e) => {
+      println!("Error: {:?}", e);
+    }
+  }
+
+  0
 }
+
+entry_point!(main);
