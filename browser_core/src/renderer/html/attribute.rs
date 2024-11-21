3237ea5a1e61d5de2c26a9aa4025c1ea21344afd
wyoheiii <seikimatu4649@gmail.com>
2024-11-22 04:15:17 +0900

    add attribute method

--- browser_core/src/renderer/html/attribute.rs
+++ browser_core/src/renderer/html/attribute.rs
@@ -6,3 +6,29 @@ pub struct Attribute {
     pub name: String,
     pub value: String,
 }
+
+impl Attribute {
+  pub fn new() -> Self {
+      Self {
+        name: String::new(),
+        value: String::new()
+      }
+  }
+
+  //論理的凝集度になってるからadd_to_name, add_to_valueのほうがよさそう
+  pub fn add_char(&mut self, c: char, is_name: bool) {
+    if is_name {
+      self.name.push(c);
+    } else {
+      self.value.push(c);
+    }
+  }
+
+  pub fn name(&self) -> String {
+    self.name.clone()
+  }
+
+  pub fn value(&self) -> String {
+    self.value.clone()
+  }
+}
