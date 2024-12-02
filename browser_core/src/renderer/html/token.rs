dbeb2d109b64393189848f958aa15928e93add7e
wyoheiii <seikimatu4649@gmail.com>
2024-12-02 22:18:04 +0900

    add token iter

--- browser_core/src/renderer/html/token.rs
+++ browser_core/src/renderer/html/token.rs
@@ -1,4 +1,4 @@
-use alloc::string::String;
+use alloc::string::{String, ToString};
 use alloc::vec::Vec;
 
 use crate::renderer::html::attribute::Attribute;
@@ -67,6 +67,311 @@ pub struct HtmlTokenizer {
   buf: String,
 }
 
+impl Iterator for HtmlTokenizer {
+  type Item = HtmlToken;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    if self.pos >= self.input.len() {
+      return None;
+    }
+
+    loop {
+      let c  = self.consume_next_input();
+
+      match self.state {
+        State::Data => {
+          if c == '<' {
+            self.state = State::TagOpen;
+          }
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+          return Some(HtmlToken::Char(c));
+        }
+
+        State::TagOpen => {
+          if c == '/' {
+            self.state = State::EndTagOpen;
+            continue;
+          }
+          if c.is_ascii_alphabetic() {
+            self.reconsume = true;
+            self.state = State::TagName;
+            self.create_tag(true);
+            continue;
+          }
+        }
+
+        State::EndTagOpen => {
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          if c.is_alphabetic() {
+            self.reconsume = true;
+            self.state = State::TagName;
+            self.create_tag(false);
+            continue;
+          }
+        }
+
+        State::TagName => {
+          if c == ' ' {
+            self.state = State::BeforeAttributeName;
+            continue;
+          }
+          if c == '/' {
+            self.state = State::SelfClosingStartTag;
+            continue;
+          }
+          if c == '>' {
+            self.state = State::Data;
+            return self.take_latest_token();
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+          self.append_tag_name(c);
+        }
+
+        State::BeforeAttributeName => {
+          if c == '/' || c == '>' || self.is_eof() {
+            self.reconsume = true;
+            self.state = State::AfterAttributeName;
+            continue;
+          }
+
+          self.reconsume = true;
+          self.state = State::AttributeName;
+          self.start_new_attribute();
+        }
+
+        State::AttributeName => {
+          if c == ' ' || c == '/' || c == '>' || self.is_eof() {
+            self.reconsume = true;
+            self.state = State::AfterAttributeName;
+            continue;
+          }
+
+          if c == '=' {
+            self.state = State::BeforeAttributeValue;
+            continue;
+          }
+
+          if c.is_ascii_uppercase() {
+            self.append_attribute(c.to_ascii_lowercase(),
+                                              /*is_name*/ true);
+            continue;
+          }
+
+          self.append_attribute(c, /*is_name*/ true);
+        }
+
+        State::AfterAttributeName => {
+          if c == ' ' {
+            continue;
+          }
+
+          if c == '/' {
+            self.state = State::SelfClosingStartTag;
+            continue;
+          }
+
+          if c == '=' {
+            self.state = State::BeforeAttributeValue;
+          }
+
+          if c == '>' {
+            self.state = State::Data;
+            return self.take_latest_token();
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.reconsume = true;
+          self.state = State::AttributeName;
+          self.start_new_attribute();
+        }
+
+        State::BeforeAttributeValue => {
+          if c == ' ' {
+            continue;
+          }
+
+          if c == '"' {
+            self.state = State::AttributeValueDoubleQuoted;
+            continue;
+          }
+
+          if c == '\'' {
+            self.state = State::AttributeValueSingleQuoted;
+            continue;
+          }
+
+          self.reconsume = true;
+          self.state = State::AttributeValueUnquoted;
+        }
+
+        State::AttributeValueDoubleQuoted => {
+          if c == '"'{
+            self.state = State::AfterAttributeValueQuoted;
+            continue;
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.append_attribute(c, /*is_name*/ false);
+        }
+
+        State::AttributeValueSingleQuoted => {
+          if c == '\'' {
+            self.state = State::AfterAttributeValueQuoted;
+            continue;
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.append_attribute(c, /*is_name*/ false);
+        }
+
+        State::AttributeValueUnquoted => {
+          if c == ' ' {
+            self.state = State::BeforeAttributeName;
+            continue;
+          }
+
+          if c == '>' {
+            self.state = State::Data;
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.append_attribute(c, /*is_name*/ false);
+        }
+
+        State::AfterAttributeValueQuoted => {
+          if c == ' ' {
+            self.state = State::BeforeAttributeName;
+            continue;
+          }
+
+          if c == '/' {
+            self.state = State::SelfClosingStartTag;
+            continue;
+          }
+
+          if c == '>' {
+            self.state = State::Data;
+            return self.take_latest_token();
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          self.reconsume = true;
+          self.state = State::BeforeAttributeValue;
+        }
+
+        State::SelfClosingStartTag => {
+          if c == '>' {
+            self.set_self_closing_flag();
+            self.state = State::Data;
+            return self.take_latest_token();
+          }
+
+          if self.is_eof() {
+            // invalid parse error.
+            return Some(HtmlToken::Eof);
+          }
+        }
+
+        State::ScriptData => {
+          if c == '<' {
+            self.state = State::ScriptDataLessThanSign;
+            continue;
+          }
+
+          if self.is_eof() {
+            return Some(HtmlToken::Eof);
+          }
+
+          return Some(HtmlToken::Char(c));
+        }
+
+        State::ScriptDataLessThanSign => {
+          if c == '/' {
+            //一時的なバッファを空文字でリセットする
+            self.buf = String::new();
+            self.state = State::ScriptDataEndTagOpen;
+            continue;
+          }
+
+          self.reconsume = true;
+          self.state = State::ScriptData;
+          return Some(HtmlToken::Char('<'));
+        }
+
+        State::ScriptDataEndTagOpen => {
+          if c.is_ascii_alphabetic() {
+            self.reconsume = true;
+            self.state = State::ScriptDataEndTagName;
+            self.create_tag(false);
+            continue;
+          }
+
+          self.reconsume = true;
+          self.state = State::ScriptData;
+          // 仕様では "<" と "/"の２つの文字トークンを返すとなっているが、
+          // この実装ではnextメソッドからは一つのトークンしか返せないため、"<"のトークンのみを返す
+          return Some(HtmlToken::Char('<'));
+        }
+
+        State::ScriptDataEndTagName => {
+          if c == '>' {
+            self.state = State::Data;
+            return self.take_latest_token();
+          }
+
+          if c.is_ascii_alphabetic() {
+            self.buf.push(c);
+            self.append_tag_name(c.to_ascii_lowercase());
+            continue;
+          }
+
+          self.state = State::TemporaryBuffer;
+          self.buf = String::from("</".to_string()) + &self.buf;
+          self.buf.push(c);
+          continue;
+        }
+
+        State::TemporaryBuffer => {
+          self.reconsume = true;
+
+          if self.buf.chars().count() == 0 {
+            self.state = State::ScriptData;
+            continue;
+          }
+
+          // 最初の１文字を削除する
+          let c = self.buf.chars().nth(0).expect("self.buf should at least 1 char");
+          self.buf.remove(0);
+          return Some(HtmlToken::Char(c));
+        }
+      }
+    }
+  }
+}
+
 impl HtmlTokenizer {
   pub fn new(html: String) -> Self {
     Self {
@@ -78,5 +383,108 @@ impl HtmlTokenizer {
       buf: String::new(),
     }
   }
+
+  fn is_eof(&self) -> bool {
+    self.pos > self.input.len()
+  }
+
+  fn consume_next_input(&mut self) -> char {
+    let c = self.input[self.pos];
+    self.pos += 1;
+    c
+  }
+
+  fn create_tag(&mut self, start_tag_token: bool) {
+    if start_tag_token {
+      self.latest_token = Some(HtmlToken::StartTag {
+        tag: String::new(),
+        self_closing: false,
+        attributes: Vec::new(),
+      });
+    } else {
+      self.latest_token = Some(HtmlToken::EndTag {
+        tag: String::new(),
+      })
+    }
+  }
+
+  fn append_tag_name(&mut self, c: char) {
+    assert!(self.latest_token.is_some());
+
+    if let Some(t) = self.latest_token.as_mut() {
+      match t {
+        HtmlToken::StartTag {
+          ref mut tag,
+          self_closing: _,
+          attributes: _,
+        }
+        | HtmlToken::EndTag { ref mut tag } => tag.push(c),
+        _ => panic!("`latest_token` should be either StartTag or EndTag")
+      }
+    }
+  }
+
+  fn take_latest_token(&mut self) -> Option<HtmlToken> {
+    assert!(self.latest_token.is_some());
+
+    let t = self.latest_token.as_ref().cloned();
+    self.latest_token = None;
+    assert!(self.latest_token.is_none());
+
+    t
+  }
+
+  fn start_new_attribute(&mut self) {
+    assert!(self.latest_token.is_some());
+
+    if let Some(t) = self.latest_token.as_mut() {
+      match t {
+        HtmlToken::StartTag {
+          tag: _,
+          self_closing: _,
+          ref mut attributes,
+        } => {
+          attributes.push(Attribute::new());
+        }
+        _ => panic!("`latest_token` should be StartTag")
+      }
+    }
+  }
+
+  fn append_attribute(&mut self, c:char, is_name: bool) {
+    assert!(self.latest_token.is_some());
+
+    if let Some(t) = self.latest_token.as_mut() {
+      match t {
+        HtmlToken::StartTag {
+          tag: _,
+          self_closing: _,
+          ref mut attributes,
+        } => {
+          let len = attributes.len();
+          assert!(len > 0);
+
+          attributes[len - 1].add_char(c, is_name);
+        },
+        _ => panic!("`latest_token` should be either StartTag")
+      }
+    }
+  }
+
+  fn set_self_closing_flag(&mut self) {
+    assert!(self.latest_token.is_some());
+
+    if let Some(t) = self.latest_token.as_mut() {
+      match t {
+        HtmlToken::StartTag {
+          tag:_,
+          ref mut self_closing,
+          attributes:_,
+        } => *self_closing = true,
+        _ => panic!("`latest_token` should be StartTag"),
+      }
+    }
+  }
+
 
 }
